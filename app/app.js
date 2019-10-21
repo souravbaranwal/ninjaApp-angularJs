d36db9f70a9088db0a4d79d3040c38d086033c5d
sourav <sourav.baranwal@gmail.com>
2019-10-21 11:40:46 +0530

    set proper route with $locationProvider

--- app/app.js
+++ app/app.js
@@ -2,7 +2,10 @@ var myNinjaApp = angular.module("myNinjaApp", ["ngRoute", "ngAnimate"]);
 
 myNinjaApp.config([
   "$routeProvider",
-  function($routeProvider) {
+  "$locationProvider",
+  function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
     $routeProvider
       .when("/home", {
         templateUrl: "views/home.html",
